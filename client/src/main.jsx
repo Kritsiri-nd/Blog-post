b74326f2c2199c20dbcc53db4766c52bae616ca2
Kritsiri <kritsiri-nd@gmail.com>
2025-10-05 19:39:18 +0700

    feat: implement authentication and protected routes
    - Refactored the App component to include authentication and protected routes using the new AuthenticationRoute and ProtectedRoute components.
    - Integrated loading screens for better user experience during authentication processes.
    - Created a context for managing authentication state and user roles, enhancing route protection based on user authentication status.
    - Added a jwtInterceptor to manage token-based authentication for API requests.
    - Introduced a LoadingScreen component for displaying loading states across the application.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/authentication.jsx";
+import jwtInterceptor from "./utils/jwtInterceptor.js";
 
-createRoot(document.getElementById('root')).render(
+// เรียกใช้ jwtInterceptor ก่อนที่แอปจะเริ่มทำงาน
+jwtInterceptor();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
+
