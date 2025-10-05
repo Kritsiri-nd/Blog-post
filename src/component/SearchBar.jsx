66f1cf1383a2ce13f3a2016e517742a3142bb51e
Kritsiri <kritsiri-nd@gmail.com>
2025-10-05 08:29:18 +0700

    fix: update styling in ArticleSection and SearchBar components
    - Adjusted the layout in ArticleSection to improve visual hierarchy by adding a background color to the article container.
    - Enhanced the SearchBar component by changing the background color to white for better contrast and readability.

--- src/component/SearchBar.jsx
+++ src/component/SearchBar.jsx
@@ -134,7 +134,7 @@ const SearchBar = () => {
             }
           }}
           placeholder="Search"
-          className="w-full max-w-[350px] px-4 py-2 pl-10 pr-4 text-sm border border-brown-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green focus:border-transparent"
+          className="w-full max-w-[350px] px-4 py-2 pl-10 pr-4 text-sm border border-brown-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green focus:border-transparent bg-white"
         />
         <img
           src={SearchIcon}
