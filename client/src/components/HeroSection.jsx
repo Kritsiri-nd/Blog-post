8a606d43b55f5932222b1137f3eee62aa410dc7f
Kritsiri <kritsiri-nd@gmail.com>
2025-10-13 19:58:22 +0700

    refactor: improve layout and styling in HeroSection and Navbar components
    - Updated padding classes in HeroSection for better responsiveness on mobile and desktop views.
    - Refined mobile navigation in Navbar by adjusting the dropdown width and ensuring consistent styling for user profile actions.
    - Enhanced mobile login/signup buttons for improved user experience and accessibility.

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import authorImage from '../assets/authorPhoto.jpg'
 function HeroSection() {
   return (
     <section className='flex sm:flex-row flex-col gap-4 p-10  w-full mx-auto max-w-[1200px] justify-center'>
-      <div className='sm:text-right text-center justify-center flex flex-col sm:max-w-[400px] pr-10'>
+      <div className='sm:text-right text-center justify-center flex flex-col sm:max-w-[400px] sm:pr-10'>
         <h1 className="h1 pb-6 hidden sm:block">Stay<br></br> Informed, <br></br>Stay Inspired</h1>
         <h1 className="h2 pb-6 sm:hidden">Stay Informed, <br></br>Stay Inspired</h1>
         <p className='text-brown-400 b1'>Discover a World of Knowledge at Your <br></br>Fingertips. Your Daily Dose of Inspiration <br></br>and Information.</p>
@@ -11,7 +11,7 @@ function HeroSection() {
       <div>
         <img src={authorImage} alt='authorImage' className='object-cover w-full h-130 sm:max-w-[400px] '></img>
       </div>
-      <div className='text-left justify-center flex flex-col max-w-[400px] pl-10'>
+      <div className='text-left justify-center flex flex-col max-w-[400px] sm:pl-10'>
         <h1 className='b3 pb-4 text-brown-400'>-Author</h1>
         <h1 className='h3 pb-6'>Thompson P.</h1>
         <p className='b1 pb-6 text-brown-400'>I am a pet enthusiast and freelance writer who specializes in animal behavior and care. With a deep love for cats, I enjoy sharing insights on feline companionship and wellness.</p>
